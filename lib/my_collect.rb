70b8d89e613b25796f031f1ce529d267d99ccef4
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-23 03:34:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+list = ["Tom Jones", "Bob Costas", "Don Knotts"]
 
-
+def my_collect(array)
+  i = 0
+  name_collection = []
+    while i < array.length
+      name_collection.push yield(array[i])
+    end
+  end
+end
